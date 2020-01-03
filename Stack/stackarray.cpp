01edf25bd8d63afc26be7bf6352ceb547a9dc80d
seaque <45668726+seaque@users.noreply.github.com>
2020-01-03 14:28:16 +0300

    Update stackarray.cpp

--- Stack/stackarray.cpp
+++ Stack/stackarray.cpp
@@ -4,36 +4,36 @@
 using namespace std;
 
 struct st {
-	int boyut;
+	int size;
 	int top;
-	int *d;
+	int *arr;
 };
 
 st *create() 
 {
 	st *s =	new st;
-	s->boyut = 2;
+	s->size = 2;
 	s->top = -1;
-	s->d = NULL;
+	s->arr = NULL;
 	return s;
 }
 
 void push(st *s, int x)
 {
 	if (s->top == -1)
-		s->d = new int[s->boyut];
-	if (s->top >= s->boyut)
+		s->arr = new int[s->size];
+	if (s->top >= s->size)
 	{
-		int *d2;
-		d2 = new int[s->boyut*2];
+		int *tmparr;
+		tmparr = new int[s->size*2];
 		for (int i=0; i <= s->top; i++)
-			d2[i] = s->d[i];
-		s->d = d2;
-		d2 = NULL;
-		delete d2;
-		s->boyut = s->boyut*2;
+			tmparr[i] = s->arr[i];
+		s->arr = tmparr;
+		tmparr = NULL;
+		delete tmparr;
+		s->size = s->size*2;
 	}
-	s->d[++(s->top)] = x;
+	s->arr[++(s->top)] = x;
 }
 
 int pop(st *s)
@@ -42,25 +42,25 @@ int pop(st *s)
 		cout << "Stack bos." << " ";
 	else
 	{
-		if (s->top <= s->boyut/2)
+		if (s->top <= s->size/2)
 		{
-			int *d2;
-			d2 = new int[s->boyut/2];
+			int *tmparr;
+			tmparr = new int[s->size/2];
 			for (int i=0; i <= s->top; i++)
-				d2[i] = s->d[i];
-			s->d = d2;
-			d2 = NULL;
-			delete d2;
-			s->boyut = s->boyut/2;
+				tmparr[i] = s->arr[i];
+			s->arr = tmparr;
+			tmparr = NULL;
+			delete tmparr;
+			s->size = s->size/2;
 		}
 	}
-	return s->d[(s->top)--];
+	return s->arr[(s->top)--];
 }
 
-void print(st *s)
+void display(st *s)
 {
 	for (int i = s->top; i > -1; i--)
-		cout << s->d[i] << "\n";
+		cout << s->arr[i] << "\n";
 }
 
 int main()
@@ -68,12 +68,14 @@ int main()
 	st *stack1, *stack2;
 	stack1 = create();
 	stack2 = create();
-	for (int i=0; i < 5; i++)
-		push(stack1, i);		
-	push(stack2, pop(stack1));
-	print(stack1);
-	print(stack2);
-	
+	for (int i=0; i < 10; i++)
+		push(stack1, i);	
+    for (int i=0; i < 4; i++)
+    	push(stack2, pop(stack1));
+
+	display(stack1);
+    system("pause");
+	display(stack2);
 	system("pause");
 	return 0;
 }
